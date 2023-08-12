f72dc4c6deb31a0a74aba71b983f808ff699ea72
laavanya11 <laavanya113@gmail.com>
2023-08-13 00:17:54 +0530

    Calcultor-Complete

---
+++ script.js
@@ -0,0 +1,38 @@
+//for input value
+let =document.getElementById('input-data');
+//for buttons selected
+let buttons=document.querySelectorAll('button') ;
+//an empty string is taken
+let string="";  
+//making array of all buttons using frunction Array.from()
+let arr=Array.from(buttons);
+
+arr.forEach(button =>{      
+    button.addEventListener('click' , (e)=>{
+
+        if(e.target.innerHTML=="." && string.includes('.'))  //checks if mutiple times continuous decimal is not pressed
+        return;
+
+        if(e.target.innerHTML == '=')
+        {
+            string= eval(string);      //inbuilt function to evalute
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC')
+        {
+            string="";                //clear string
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL')
+        {
+            string= string.substring(0, string.length()-1);  //inbuilt function to get substring
+            input.value = string;
+        }
+        else
+        {
+            string += e.target.innerHTML;        //add input to string
+            input.value = string;
+        }
+    })
+})
+
